7943cb7fa876b84fdf329badc75d1e2a425da8d6
JoeHughes-IBM <jahughes@us.ibm.com>
2017-03-03 15:09:03 -0500

    Update for small issue

--- index.php
+++ index.php
@@ -20,7 +20,7 @@ function loadItems(items){
     var i = 0;
     console.log("Load Items: " + items.rows);
     document.getElementById("loading").innerHTML = "";
-    for(i = items.rows.length; i > 1; i--){
+    for(i = items.rows.length; i < 1; i--){
         addItem(items.rows[i].doc, i);
     }
 }
