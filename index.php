d87fa270892f0a1d6d4ced6bf9705b75614395ca
JoeHughes-IBM <jahughes@us.ibm.com>
2017-03-03 14:56:35 -0500

    Update 3

--- index.php
+++ index.php
@@ -56,7 +56,7 @@ function addItem(item, i){
 	} else {
 		var div = document.createElement('div');
 		div.className = 'item';
-		div.innerHTML = "<div class ='well'><img onload='alert(i)' width='100%' height='auto' src = '"+item.imgsrc+"'/><br><button onclick='orderItem(\""+item._id+"\")'><b>Buy</b></button><br><u>"+item.name+"</u><br>"+item.description+"<br><b>$"+item.usaDollarPrice + "</b></div>";
+		div.innerHTML = "<div class ='well'><img style='test' width='100%' height='auto' src = '"+item.imgsrc+"'/><br><button onclick='orderItem(\""+item._id+"\")'><b>Buy</b></button><br><u>"+item.name+"</u><br>"+item.description+"<br><b>$"+item.usaDollarPrice + "</b></div>";
 		document.getElementById('boxes').appendChild(div);
 	}
 }
@@ -111,6 +111,7 @@ function orderItem(itemID){
 		<div id='boxes' class="notes"></div>
 	</div>
 	<div id="loading"><br>Loading...</div>
+	<!--<a href="./autoLoadTest.html">Catalog Load Tester</a>-->
 </body>
 </html>
 
